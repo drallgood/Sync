1909bdd31823c92269574eb9bab3a3cec412d9f2
Patrice Brend'amour <patrice@brendamour.net>
2021-12-30 18:16:56 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "Sync",
+     
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Sync",
+            targets: ["Sync"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Sync",
+                dependencies: ["CoreData"],
+                path: "Sources"
+        ),
+
+    ]
+)
